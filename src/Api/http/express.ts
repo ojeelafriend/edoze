ca307f9d5a205044304aba440994ec7bdf42cee4
odev <ts8irolon@gmail.com>
2024-03-06 11:36:59 -0300

    [create] configuracion de express base para el manejo de rutas http

---
+++ src/Api/http/express.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json({ limit: '50mb' }), express.urlencoded({ extended: false }));
+app.use(express.static('./public'));
+
+// injection routes.
+//here...
+
+export default app;
