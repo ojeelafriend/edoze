4758511f6d4f36adaffdfcc7b60d40b8b9ea0c27
odev:secondary <ojeelafriend@gmail.com>
2024-04-07 17:15:48 -0300

    [create] Entity Buyer con validaciones y metodo de creacion

---
+++ src/Ecommerce/domain/Buyer.ts
@@ -0,0 +1,69 @@
+import { uuid as uuidv4 } from "uuidv4";
+
+import { Product } from "./Product";
+
+export class Buyer {
+  private readonly MIN_CHAR_USERNAME: number = process.env.MIN_USERNAME
+    ? parseInt(process.env.MIN_USERNAME)
+    : 4;
+
+  private readonly MAX_CHAR_USERNAME: number = process.env.MAX_USERNAME
+    ? parseInt(process.env.MAX_USERNAME)
+    : 14;
+
+  private readonly MIN_CHAR_PASSWORD: number = process.env.MAX_PASSWORD
+    ? parseInt(process.env.MAX_PASSWORD)
+    : 6;
+
+  private readonly uuid: string;
+  private readonly username: string;
+  private readonly password: string;
+  private readonly shoppingCart: Product[];
+
+  public constructor(
+    username: string,
+    password: string,
+    shoppingCart: Product[]
+  ) {
+    this.uuid = uuidv4();
+    this.username = username;
+    this.password = password;
+    this.shoppingCart = shoppingCart;
+  }
+
+  public create(
+    username: string,
+    password: string,
+    shoppingCart: Product[] = []
+  ) {
+    const containSpace: RegExp = /\s/;
+    const checkString: RegExp = /^[a-zA-Z0-9]+$/;
+
+    if (username.length >= this.MIN_CHAR_USERNAME) {
+      throw new Error(
+        `Username requires at least ${this.MIN_CHAR_USERNAME} characters`
+      ).message;
+    }
+
+    if (username.length > this.MAX_CHAR_USERNAME) {
+      throw new Error(`Character range exceeded for username`).message;
+    }
+
+    if (containSpace.test(username)) {
+      throw new Error(`This username can't contain characters with space`)
+        .message;
+    }
+
+    if (!checkString.test(username)) {
+      throw new Error(`This username can't contain symbols`).message;
+    }
+
+    if (password.length < this.MIN_CHAR_PASSWORD) {
+      throw new Error(
+        `This password requires at least ${this.MIN_CHAR_PASSWORD} characters`
+      );
+    }
+
+    return new Buyer(username, password, shoppingCart);
+  }
+}
