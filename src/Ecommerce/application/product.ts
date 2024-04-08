f4b3b89c08978acd19c27c01b66422d53407693d
odev:secondary <ojeelafriend@gmail.com>
2024-04-07 22:05:36 -0300

    [create] casos de uso de producto: createProduct, edit, list, remove

---
+++ src/Ecommerce/application/product.ts
@@ -0,0 +1,43 @@
+import { IProductRepository } from "../domain/IProductRepository";
+import { Product } from "../domain/Product";
+
+export const createProduct = async (
+  repository: IProductRepository,
+  name: string,
+  price: number,
+  seller: string
+) => {
+  const product = Product.create({ name, price, seller });
+  await repository.create(product);
+};
+
+export const listProductById = async (
+  repository: IProductRepository,
+  uuid: string
+): Promise<Product> => {
+  return await repository.findById(uuid);
+};
+
+export const listAllProduct = async (
+  repository: IProductRepository
+): Promise<Product[]> => {
+  return await repository.findAll();
+};
+
+export const editProduct = async (
+  repository: IProductRepository,
+  uuid: string,
+  name: string,
+  price: number,
+  seller: string
+): Promise<void> => {
+  const product = Product.create({ uuid, name, price, seller });
+  await repository.update(product.getId(), product);
+};
+
+export const removeProduct = async (
+  repository: IProductRepository,
+  uuid: string
+): Promise<void> => {
+  await repository.delete(uuid);
+};
