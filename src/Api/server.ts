c6cffd72377fdf4fca021bf4effd5ca5791dd7bf
odev <ts8irolon@gmail.com>
2024-05-05 13:30:24 -0300

    [modify] support with graphql and simulate on REST API

--- src/Api/server.ts
+++ src/Api/server.ts
@@ -1,11 +1,25 @@
-import dotenv from 'dotenv';
+import { startStandaloneServer } from "@apollo/server/standalone";
+import dotenv from "dotenv";
 
-import App from './http/express';
-import db from '../Shared/mongoose';
+import graphqlServer from "./http/graphql/server";
+import { checkArgv } from "./util/argv";
+import db from "../Shared/mongoose";
+import App from "./http/express";
 
 dotenv.config();
 
-App.listen(process.env.EDOZE_PORT, async () => {
-  console.log('Server is ready');
-  await db.run();
-});
+const type = checkArgv();
+
+type === "rest"
+  ? App.listen(process.env.EDOZE_PORT, async () => {
+      console.log(
+        `Server REST is ready on http://localhost:${process.env.EDOZE_PORT}`
+      );
+      await db.run();
+    })
+  : startStandaloneServer(graphqlServer, { listen: { port: 4000 } }).then(
+      async ({ url }) => {
+        console.log(`Server graphql is ready on ${url}`);
+        await db.run();
+      }
+    );
